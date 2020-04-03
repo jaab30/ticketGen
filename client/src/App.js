e61d95cc2b9e3fc8f614096267d3dd026b4164e2
jaab30 <jaab30@yahoo.com>
2020-04-03 08:27:15 -0400

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
