774a452cf5d9b473311fd639789b5c3384e9447f
jaab30 <jaab30@yahoo.com>
2020-05-15 07:58:14 -0400

    add catch all route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
 const userRoutes = require("./users");
+const path = require("path");
 
 // user Routes
 router.use("/api/users", userRoutes);
