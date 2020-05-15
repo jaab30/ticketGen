ed79ccfd5857f8d4fa735e1b39d907d7b82749b8
jaab30 <jaab30@yahoo.com>
2020-05-15 07:53:29 -0400

    add catch all route

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ const userRoutes = require("./users");
 router.use("/api/users", userRoutes);
 // api Routes
 router.use("/api", apiRoutes);
+// If no API routes are hit, send the React app
+router.use((req, res) =>
+    res.sendFile(path.join(__dirname, "../client/build/index.html"))
+);
 
 
 module.exports = router;
