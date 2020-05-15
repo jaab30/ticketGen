6c16f26f189e309ab5fd29deda3aba481d031289
jaab30 <jaab30@yahoo.com>
2020-05-15 13:01:39 -0400

    Added setupProxy component and delete proxy from package.json

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,5 +2,5 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = app => {
-    app.use(createProxyMiddleware("/api", { target: "http://localhost:8000" }))
+    app.use(createProxyMiddleware("/api", { target: "http://localhost:4000" }))
 }
