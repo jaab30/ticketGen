6ecd0c0a12cef31204b73a24b59949829d51804c
jaab30 <jaab30@yahoo.com>
2020-05-15 16:50:20 -0400

    Deployment issues

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-    app.use('/api', createProxyMiddleware({ target: 'https://tix-generator.herokuapp.com/' }));
+    app.use('/api', createProxyMiddleware({ target: "http://localhost:3001/" }));
 
 }
