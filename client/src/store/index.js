86bef617d049324db46d7d021f1cf00423befcd9
jaab30 <jaab30@yahoo.com>
2020-05-15 21:56:13 -0400

    Update dev tools

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,7 +3,7 @@ import allReducers from "../reducers";
 import thunk from "redux-thunk";
 
 
-const store = createStore(allReducers, applyMiddleware(thunk));
-// const store = createStore(allReducers, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+// const store = createStore(allReducers, applyMiddleware(thunk));
+const store = createStore(allReducers, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (a) => a));
 
 export default store;
