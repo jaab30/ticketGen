2c57ba164cb642e8ebf67869c1de3c59f8f5cd71
jaab30 <jaab30@yahoo.com>
2020-05-15 21:50:23 -0400

    Remove dev tools

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,8 +1,9 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import allReducers from "../reducers";
 import thunk from "redux-thunk";
 
 
-const store = createStore(allReducers, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(allReducers, applyMiddleware(thunk));
+// const store = createStore(allReducers, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 export default store;
