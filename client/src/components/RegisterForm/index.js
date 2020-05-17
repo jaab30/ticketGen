246669e458f1d22ef11d8e96e6775614935e036f
jaab30 <jaab30@yahoo.com>
2020-05-17 14:36:37 -0400

    Fix PhoneNumber Register Bug

--- client/src/components/RegisterForm/index.js
+++ client/src/components/RegisterForm/index.js
@@ -75,13 +75,14 @@ function RegisterForm() {
 
         dispatch(register(dataObj));
     }
-    const formatPhone = (phoneNumberString) => {
+    const formatPhone = (phoneNumberString = "") => {
         const cleaned = ('' + phoneNumberString).replace(/\D/g, '')
         const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
         if (match) {
             return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+        } else {
+            return phoneNumberString;
         }
-        return ""
     }
 
 
