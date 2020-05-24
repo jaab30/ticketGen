ffd8a79e4147c996b876bdf5a3450d393fc68303
jaab30 <jaab30@yahoo.com>
2020-05-24 18:26:31 -0400

    AWS AC2 Deploy

--- config/config.js
+++ config/config.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 
 module.exports =
 {
-    MONGO_URI: "mongodb+srv://jaab30:jaab30@ticketgen-6bfeu.mongodb.net/tixgen?retryWrites=true&w=majority",
-    jwtSecret: "tix_gen_123"
+    MONGO_URI: process.env.MONGO_URI,
+    jwtSecret: process.env.JWT_SECRET
 }
 
 
